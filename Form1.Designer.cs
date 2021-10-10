eb3ebc7c22a3c1009a12ae84e4339cf6c84f3eb6
samurainisa <aressamurai@mail.ru>
2021-10-10 21:02:55 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonras = new System.Windows.Forms.Button();
+            this.labelmin = new System.Windows.Forms.Label();
+            this.labelmin1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Name = "label4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // zedGraphControl1
+            // 
+            resources.ApplyResources(this.zedGraphControl1, "zedGraphControl1");
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(75)))), ((int)(((byte)(105)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem,
+            this.backToolStripMenuItem,
+            this.nextToolStripMenuItem,
+            this.закрытьToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            this.menuStrip1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.menuStrip1_MouseDown);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            resources.ApplyResources(this.очиститьToolStripMenuItem, "очиститьToolStripMenuItem");
+            this.очиститьToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // backToolStripMenuItem
+            // 
+            resources.ApplyResources(this.backToolStripMenuItem, "backToolStripMenuItem");
+            this.backToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.шагВперёдToolStripMenuItem_Click);
+            // 
+            // nextToolStripMenuItem
+            // 
+            resources.ApplyResources(this.nextToolStripMenuItem, "nextToolStripMenuItem");
+            this.nextToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.шагНазадToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            resources.ApplyResources(this.закрытьToolStripMenuItem, "закрытьToolStripMenuItem");
+            this.закрытьToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // buttonras
+            // 
+            this.buttonras.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.buttonras, "buttonras");
+            this.buttonras.Name = "buttonras";
+            this.buttonras.UseVisualStyleBackColor = false;
+            this.buttonras.Click += new System.EventHandler(this.buttonras_Click);
+            // 
+            // labelmin
+            // 
+            resources.ApplyResources(this.labelmin, "labelmin");
+            this.labelmin.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelmin.Name = "labelmin";
+            // 
+            // labelmin1
+            // 
+            resources.ApplyResources(this.labelmin1, "labelmin1");
+            this.labelmin1.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelmin1.Name = "labelmin1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(43)))), ((int)(((byte)(60)))));
+            this.Controls.Add(this.labelmin1);
+            this.Controls.Add(this.labelmin);
+            this.Controls.Add(this.buttonras);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.Button buttonras;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.Label labelmin;
+        private System.Windows.Forms.Label labelmin1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+
+        List<List<PointPairList>> minList = new List<List<PointPairList>>();
+        PointPairList list = new PointPairList();
+        PointPairList list2 = new PointPairList();
+        double x1, x2;
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GraphPane graphfield = zedGraphControl1.GraphPane;
+            // Установим цвет рамки для всего компонента
+            graphfield.Border.Color = Color.Black;
+            // Установим цвет рамки вокруг графика
+            graphfield.Chart.Border.Color = Color.Black;
+            // Закрасим фон всего компонента ZedGraph
+            // Заливка будет сплошная
+            graphfield.Fill.Type = FillType.Solid;
+            graphfield.Fill.Color = Color.FromArgb(50, 49, 69);
+            // Закрасим область графика (его фон) в черный цвет
+            graphfield.Chart.Fill.Type = FillType.Solid;
+            graphfield.Chart.Fill.Color = Color.Black;
+            // Включим показ оси на уровне X = 0 и Y = 0, чтобы видеть цвет осей
+            graphfield.XAxis.MajorGrid.IsZeroLine = true;
+            graphfield.YAxis.MajorGrid.IsZeroLine = true;
+            // Установим цвет осей
+            graphfield.XAxis.Color = Color.CornflowerBlue;
+            graphfield.YAxis.Color = Color.CornflowerBlue;
+            // Включим сетку
+            graphfield.XAxis.MajorGrid.IsVisible = true;
+            graphfield.YAxis.MajorGrid.IsVisible = true;
+            // Установим цвет для сетки
+            graphfield.XAxis.MajorGrid.Color = Color.FromArgb(62, 120, 138);
+            graphfield.YAxis.MajorGrid.Color = Color.FromArgb(62, 120, 138);
+            //62, 120, 138
+            // Установим цвет для подписей рядом с осями
+            graphfield.XAxis.Title.FontSpec.FontColor = Color.Silver;
+            graphfield.YAxis.Title.FontSpec.FontColor = Color.Silver;
+            // Установим цвет подписей под метками
+            graphfield.XAxis.Scale.FontSpec.FontColor = Color.Silver;
+            graphfield.YAxis.Scale.FontSpec.FontColor = Color.Silver;
+            // Установим цвет заголовка над графиком
+            graphfield.Title.FontSpec.FontColor = Color.White;
+        }
+        private async void buttonras_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            if (pane.CurveList.Count > 0)
+            {
+                MessageBox.Show("Очистите перед сменой настроек", "Ошибка");
+            }
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            list.Clear();
+            list2.Clear();
+            minList.Clear();
+            pane.CurveList.Clear();
+            pane.GraphObjList.Clear();
+            x1 = 0;
+            x2 = 0;
+
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Введите границу А");
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("Введите границу Б");
+            }
+            else if (textBox2.Text.Length >= 4 || textBox3.Text.Length >= 4)
+            {
+                MessageBox.Show("Слишком большие границы!", "Внимание!");
+            }
+            else if (textBox4.Text.Length > 9)//проверка на заполненность данных
+            {
+                MessageBox.Show("Уменьшите Е", "Ошибка!");
+            }
+            else if (double.Parse(textBox2.Text) >= double.Parse(textBox3.Text))
+            {
+                MessageBox.Show("Граница a должна быть меньше границы b");
+            }
+            else
+            {
+                    GraphPane graphfield = zedGraphControl1.GraphPane;
+                    await buildasync();
+                    string expression = textBox1.Text;
+                    double localMinX = await method(double.Parse(textBox2.Text), double.Parse(textBox3.Text), double.Parse(textBox4.Text));
+                    list2.Add(localMinX, func(localMinX));
+                    graphfield.Title.Text = "График" + " " + textBox1.Text;
+                    labelmin1.Text = Math.Round(func(localMinX), 5).ToString();
+                    index = minList.Count;
+                    addpoint();
+                    zedGraphControl1.AxisChange();
+                    zedGraphControl1.Invalidate();
+            }
+        }
+
+        public void addpoint()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.AddCurve("Минимум", list2, Color.Red, SymbolType.Diamond);
+        }
+
+        private void buildgraph()
+        {
+            try
+            {
+                double xmin, xmax;
+                GraphPane pane = zedGraphControl1.GraphPane;
+                list.Clear();
+                xmin = Convert.ToDouble(textBox2.Text);
+                xmax = Convert.ToDouble(textBox3.Text);
+
+                for (double x = xmin; x <= xmax; x += 0.1)
+                {
+                    list.Add(x, func(x));
+                }
+
+                pane.AddCurve(textBox1.Text, list, Color.Blue, SymbolType.None);
+                Action action = () => addpoint();
+                Invoke(action);
+
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+            catch (FormatException)
+            {
+                DialogResult err = MessageBox.Show("Данные неверны\nПроверьте корректность данных", "Ошибка");
+            }
+        }
+
+        private double goldenRatio(double a, double b, double e)
+        {
+            double d = (-1 + Math.Sqrt(5)) / 2;
+            while (Math.Abs(b - a) > e)
+            {
+                PointPairList xa = new PointPairList();
+                PointPairList xb = new PointPairList();
+                List<PointPairList> x1x2 = new List<PointPairList>();
+                xa.Add(a, func(a));
+                xb.Add(b, func(b));
+                x1x2.Add(xa);
+                x1x2.Add(xb);
+                minList.Add(x1x2);
+                x1 = b - (b - a) * d;
+                x2 = a + (b - a) * d;
+                if(func(x1) >= func(x2))
+                {
+                    a = x1;
+                }
+                else
+                {
+                    b = x2;
+                }
+            }
+            return (a + b) / 2;
+        }
+
+        async Task<Double> method(double a, double b, double e)//асинхроним расчеты метода
+        {
+            var result = await Task.Run(() => goldenRatio(a, b, e));
+            return result;
+        }
+        private async Task buildasync()
+        {
+            await Task.Run(() => buildgraph());
+        }
+        private double func(double x)
+        {
+            try
+            {
+                Argument xmain = new Argument("x");
+                Expression y = new Expression(textBox1.Text.Replace(',', '.'), xmain);
+                xmain.setArgumentValue(x);
+                return y.calculate();
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            list.Clear();
+            list2.Clear();
+            minList.Clear();
+            pane.CurveList.Clear();
+            pane.GraphObjList.Clear();
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45)
+            {
+                e.Handled = true;
+            }
+        }Ы
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (textBox4.Text.Length == 2)
+            {
+                if (number == 8 || !Char.IsDigit(number))
+                {
+                    e.Handled = true;
+                }
+            }
+            else
+            {
+                if (!Char.IsDigit(number) && number != 8)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char comma = e.KeyChar;
+
+            if (comma == 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void шагНазадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (list.Count == 0)
+            {
+                buildgraph();
+            }
+            else
+            {
+                GraphPane pane = zedGraphControl1.GraphPane;
+
+                if (pane.CurveList.Count == 2)
+                {
+                    pane.CurveList.RemoveAt(1);
+                }
+                else if (pane.CurveList.Count == 3)
+                {
+                    pane.CurveList.RemoveAt(1);
+                    pane.CurveList.RemoveAt(1);
+                }
+                if (index > 0)
+                {
+                    index--;
+                    pane.AddCurve("x1", minList[index][0], Color.Red, SymbolType.Diamond);
+                    pane.AddCurve("x2", minList[index][1], Color.Red, SymbolType.Diamond);
+                }
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void menuStrip1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+
+        private void шагВперёдToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (list.Count == 0)
+            {
+                buildgraph();
+            }
+            else
+            {
+                GraphPane pane = zedGraphControl1.GraphPane;
+                if (pane.CurveList.Count == 2)
+                {
+                    pane.CurveList.RemoveAt(1);
+                }
+                else if (pane.CurveList.Count == 3)
+                {
+                    pane.CurveList.RemoveAt(1);
+                    pane.CurveList.RemoveAt(1);
+                }
+
+                if (index < minList.Count - 1)
+                {
+                    index++;
+                    pane.AddCurve("x1", minList[index][0], Color.Red, SymbolType.Diamond);
+                    pane.AddCurve("x2", minList[index][1], Color.Red, SymbolType.Diamond);
+                }
+                else if (index == minList.Count - 1)
+                {
+                    pane.AddCurve("Минимум", list2, Color.Red, SymbolType.Diamond);
+                }
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+        }
+    }
+}
